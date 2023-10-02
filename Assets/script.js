1024221f92937efc877c48d127867abaf0de2a41
ba23 <blsarabia7@gmail.com>
2023-10-02 12:43:53 -0600

    redid screenshot and reformatted

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,34 @@
-const apiKey="c2d207c6dde0d257e39e705751870bb1";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-var inputText = document.getElementById ("input-text")
-var searchBtn = document.getElementById ("search-btn") 
-var temp = document.getElementById ("temp")
-var wind = document.getElementById ("wind")
-var humid = document.getElementById ("humid")
-var cityNameText = document.getElementById ("city-name-text")
-var weatherImg= document.getElementById ("icon")
+const apiKey = "c2d207c6dde0d257e39e705751870bb1";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+var inputText = document.getElementById("input-text");
+var searchBtn = document.getElementById("search-btn");
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humid = document.getElementById("humid");
+var cityNameText = document.getElementById("city-name-text");
+var weatherImg = document.getElementById("icon");
 // .textContent
 
+searchBtn.addEventListener("click", function () {
+  console.log(inputText.value);
+  checkWeather(inputText.value);
+  cityNameText.textContent = inputText.value;
+});
 
-
-searchBtn.addEventListener("click", function() {
-console.log (inputText.value) 
-checkWeather(inputText.value)
-cityNameText.textContent= inputText.value
-})
-
-async function checkWeather(cityName){
-  const response = await fetch(apiUrl + cityName +`&appid=${apiKey}`);
+async function checkWeather(cityName) {
+  const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
   var data = await response.json();
 
   console.log(data);
-  temp.textContent="Temp: " + data.main.temp + "°F"
-  wind.textContent= " Wind: "+ data.wind.speed + "MPH"
-  humid.textContent=" Humidity: " +data.main.humidity + "%"
+  temp.textContent = "Temp: " + data.main.temp + "°F";
+  wind.textContent = " Wind: " + data.wind.speed + "MPH";
+  humid.textContent = " Humidity: " + data.main.humidity + "%";
   // https://openweathermap.org/img/wn/10d@2x.png
 
-  var dataIcon= data.weather[0].icon
-  console.log (dataIcon)
-  var iconurl="https://openweathermap.org/img/wn/" + dataIcon + "@2x.png"
-  console.log(iconurl)
-  weatherImg.setAttribute("src", iconurl)
-
-  
-
-
-
-
+  var dataIcon = data.weather[0].icon;
+  console.log(dataIcon);
+  var iconurl = "https://openweathermap.org/img/wn/" + dataIcon + "@2x.png";
+  console.log(iconurl);
+  weatherImg.setAttribute("src", iconurl);
 }
--- README.md
+++ README.md
@@ -1,20 +1,17 @@
 # Weather-Dashboard
+
 Weather Dashboard
 
 ## Description
 
 Users will be able to see the weather outlook for multiple cities
 
-
-
 ## Visuals
 
-![Weather Dashboard](./Assets/images/Screen%20Shot%202023-10-02%20at%2012.27.35%20PM.png)
+![Weather Dashboard](./Assets/images/Screen%20Shot%202023-10-02%20at%2012.41.24%20PM.png)
 
 Link to deployed website: https://brendaleea.github.io/Weather-Dashboard/
 
-
-
-
 ## Support
+
 For any questions please message via Slack
